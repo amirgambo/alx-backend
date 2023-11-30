5d21e912ecfde7dc7979052cefa314c9e00a1235
amirgambo <gambo.amir@yahoo.com>
2023-11-30 22:05:44 +0100

    Queuing System in JS

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,24 @@
+#!/usr/bin/yarn dev
+/**
+ * Using push notification to log a job
+ */
+import { createQueue } from 'kue';
+
+const queue = createQueue({name: 'push_notification_code'});
+
+const job = queue.create('push_notification_code', {
+  phoneNumber: '0798317266',
+  message: 'New Job Created',
+});
+
+job
+  .on('enqueue', () => {
+    console.log('Notification job created:', job.id);
+  })
+  .on('complete', () => {
+    console.log('Notification job completed');
+  })
+  .on('failed attempt', () => {
+    console.log('Notification job failed');
+  });
+job.save();
